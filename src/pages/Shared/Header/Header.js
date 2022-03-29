aeb520f0168ff0d359b13ec65090f60da8f21eb1
Anik Dhar <aniksutradhar11@gmail.com>
2022-03-29 16:15:07 +0600

    intial commit

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <>
            <Navbar expand="lg" bg="light" variant="light">
     <Container>
-    <Navbar.Brand style={{ fontWeight: '700', textDecoration: 'none', fontSize: '1rem' }} href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand style={{ fontWeight: '700', textDecoration: 'none', fontSize: '1rem' }} href="#home">Educational Coursers</Navbar.Brand>
     <Nav className="ms-auto">
       <Nav.Link style={{ fontWeight: '700', textDecoration: 'none', fontSize: '1rem' }} href="#home">Home</Nav.Link>
       <Nav.Link style={{ fontWeight: '700', textDecoration: 'none', fontSize: '1rem' }} href="#features">All Courses</Nav.Link>
